61e7f09b97aa78b3ddda6bd1c68a2d00863f3370
Shein <shein@loftdev.com>
2019-05-23 19:07:21 -0400

    Spec library setup

---
+++ MMuze.framework/Headers/MMuze.h
@@ -0,0 +1,18 @@
+//
+//  MMuzeSdk.h
+//  MMuzeSdk
+//
+//  Copyright © 2019 MMuze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMuze.
+FOUNDATION_EXPORT double MMuzeVersionNumber;
+
+//! Project version string for MMuzeSdk.
+FOUNDATION_EXPORT const unsigned char MMuzeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMuzeSdk/PublicHeader.h>
+
+
